772c4eb5aa4cfc74bec2cd2f3f2209c0f09ea334
asfsghg <artemryzenka@gmail.com>
2025-08-29 10:30:09 +0300

    g

--- Assets/Scripts/WondowsConstant.cs
+++ Assets/Scripts/WondowsConstant.cs
@@ -1,9 +1,9 @@
 
 public static class WindowsConstant
 {
-    public const string Connection_Panel = "Conntection Panel";
-    public const string Main_Menu_Panel = "Main menu panel";
-    public const string Game_Menu_Panel = "Game menu panel";
-    public const string Find_rooms_Panel = "Find rooms panel";
-    public const string Game_Room_Panel = "Game room panel";
+    public const string Connection_Panel = "Connection Panel";
+    public const string Main_Menu_Panel = "Main Menu Panel";
+    public const string Game_Menu_Panel = "Create Menu Panel";
+    public const string Find_rooms_Panel = "Find Room Panel";
+    public const string Game_Room_Panel = "Main Menu Panel";
 }

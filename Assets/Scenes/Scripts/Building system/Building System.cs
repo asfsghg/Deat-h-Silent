c7f9a73303c3084f64bc3cde5dbf6123bef82cdd
asfsghg <artemryzenka@gmail.com>
2025-09-03 15:17:38 +0300

    вывыв

--- Assets/Scenes/Scripts/Building system/Building System.cs
+++ Assets/Scenes/Scripts/Building system/Building System.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-
 public class Build : MonoBehaviour
 {
     public LayerMask groundMask;
     [SerializeField] private GameObject BuildingOBJ;
     public bool _canBuild = true;
+    public float gridSize = 0.1f;
     
     void LateUpdate()
     {
@@ -17,22 +17,30 @@ public class Build : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit, 50f, groundMask))
         {
-            BuildingOBJ.transform.position = hit.point; 
+            Vector3 pos = hit.point;
+            
+            pos.x = Mathf.Round(pos.x / gridSize) * gridSize;
+            pos.y = Mathf.Round(pos.y / gridSize) * gridSize;
+            pos.z = Mathf.Round(pos.z / gridSize) * gridSize;
+
+            BuildingOBJ.transform.position = pos;
         }
-        
+
         if (Input.GetKeyDown(KeyCode.R))
         {
-            
-                BuildingOBJ.transform.Rotate(Vector3.forward, 90);
-            
-        
+            BuildingOBJ.transform.Rotate(Vector3.forward, 90);
         }
+
         if (Input.GetKeyDown(KeyCode.T))
         {
             BuildingOBJ.transform.Rotate(Vector3.up, -90f);
-            
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+           
+
+                Instantiate(BuildingOBJ, hit.point, BuildingOBJ.transform.rotation);
         }
     }
-    
 }
-
---
+++ Assets/Scenes/Scripts/Camera.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+public class CameraController : MonoBehaviour
+{
+    private Camera cam;
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        cam.transform.Translate(Vector3.right * horizontal);
+        cam.transform.Translate(Vector3.forward * vertical);
+
+    }
+}

f2df419c7bb241408b55e8c17233a457f3169698
bobi54y <uuu400424@gmail.com>
2025-09-04 17:18:06 +0300

    -баг

---
+++ Assets/Scenes/Scripts/CameraPro.cs
@@ -0,0 +1,35 @@
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPro : MonoBehaviour
+{
+
+    public float sensitivity = 12f;
+    public Transform playerBody;
+    public Vector3 offset;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; 
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        
+        playerBody.Rotate(Vector3.up * mouseX);
+        transform.position = playerBody.position + offset;
+    }
+}
--- Assets/Scenes/Scripts/neRun.cs
+++ Assets/Scenes/Scripts/neRun.cs
@@ -5,77 +5,15 @@ using UnityEngine;
 
 public class neRun : MonoBehaviour
 {
-    [Header("Move")]
-    public float walkSpeed = 4f;
-    public float sprintSpeed = 7f;
-    public float acceleration = 12f;   
-
-    [Header("Jump & Gravity")]
-    public float jumpHeight = 1.2f;
-    public float gravity = -18f;       
-    public float groundSnap = -2f;     
-
-    [Header("Ground Check")]
-    public Transform groundCheck;    
-    public float groundRadius = 0.25f;
-    public LayerMask groundMask;
-
-    private CharacterController cc;
-    private Vector3 velocity;          
-    private Vector3 currentMove;       
-
-    void Start()
-    {
-        cc = GetComponent<CharacterController>();
-        if (groundCheck == null)
-        {
-           
-            GameObject gc = new GameObject("GroundCheck");
-            gc.transform.SetParent(transform);
-            gc.transform.localPosition = new Vector3(0, -cc.height * 0.5f, 0);
-            groundCheck = gc.transform;
-        }
-    }
+    public float moveSpeed = 5f;
 
     void Update()
     {
-        
-        float x = Input.GetAxisRaw("Horizontal");   
-        float z = Input.GetAxisRaw("Vertical");     
-        Vector3 inputDir = (transform.right * x + transform.forward * z).normalized;
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-        
-        float targetSpeed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed;
-        Vector3 targetMove = inputDir * targetSpeed;
-
-       
-        currentMove = Vector3.MoveTowards(currentMove, targetMove, acceleration * Time.deltaTime);
-
-       
-        bool isGrounded = Physics.CheckSphere(groundCheck.position, groundRadius, groundMask, QueryTriggerInteraction.Ignore);
-
-        if (isGrounded && velocity.y < 0f)
-            velocity.y = groundSnap;
-
-
-        if (isGrounded && Input.GetButtonDown("Jump"))
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-
-
-        velocity.y += gravity * Time.deltaTime;
-
-        
-        Vector3 motion = currentMove * Time.deltaTime + Vector3.up * velocity.y * Time.deltaTime;
-        cc.Move(motion);
+        Vector3 move = transform.right * x + transform.forward * z;
+        transform.Translate(move * moveSpeed * Time.deltaTime, Space.World);
     }
 
-    
-    void OnDrawGizmosSelected()
-    {
-        if (groundCheck)
-        {
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawWireSphere(groundCheck.position, groundRadius);
-        }
-    }
 }

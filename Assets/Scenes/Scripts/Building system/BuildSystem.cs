64bab68c3a887255b1b7c515348304207b15eec1
asfsghg <artemryzenka@gmail.com>
2025-09-19 12:17:48 +0300

    +bag - bag

--- Assets/Scenes/Scripts/Building system/BuildSystem.cs
+++ Assets/Scenes/Scripts/Building system/BuildSystem.cs
@@ -9,28 +9,61 @@ public class BuildSystem : MonoBehaviour
     [SerializeField] private GameObject Stairs;
     [SerializeField] private GameObject Wall;
     [SerializeField] private GameObject Window;
-    
+    private GameObject _spawn;
     private bool _canBuild = true;
-    
-    void Update()
+    private bool _IsSpawned = false;
+    [SerializeField] private LayerMask groundMask;
+    void LateUpdate()
     {
         
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 10f, groundMask))
+        {
+            _spawn.transform.position = hit.point;
+            if (Input.GetMouseButtonDown(0))
+            {
+                
+                Instantiate(_spawn, hit.point, _spawn.transform.rotation);
+                Debug.Log(11111);
+                _IsSpawned = false;
+            }
+        }
     }
+   
 
     public void BaseplateSpawn()
     {
-        
+        if (_IsSpawned == false)
+        {
+            _spawn = Instantiate(Baseplate);
+            _IsSpawned = true;
+        }
+       
     }
     public void StairsSpawn()
     {
-        
+        if (_IsSpawned == false)
+        {
+            _spawn = Instantiate(Baseplate);
+            _IsSpawned = true;
+        }
     }
     public void WallSpawn()
     {
-        
+        if (_IsSpawned == false)
+        {
+            _spawn = Instantiate(Baseplate);
+            _IsSpawned = true;
+        }
     }
     public void WindowSpawn()
     {
-        
+        if (_IsSpawned == false)
+        {
+            _spawn = Instantiate(Baseplate);
+            _IsSpawned = true;
+        }
     }
+    
 }

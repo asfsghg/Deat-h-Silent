d97598707d91d4ed8b6b0ed6cb305631c2bab687
bobi54y <uuu400424@gmail.com>
2025-09-19 18:27:30 +0300

    t

--- Assets/Scenes/Scripts/Player/hp.cs
+++ Assets/Scenes/Scripts/Player/hp.cs
@@ -5,19 +5,38 @@ using UnityEngine.UI;
 
 public class hpp : MonoBehaviour
 {
-    Image healtBar;
+     public Image healtBar;
     public float maxHP = 100f;
     public float HP;
-    // Start is called before the first frame update
+    public Canvas deathCanvas;
+    private bool alreadyDied = false;
+
+    //public bool isDead = false;
+
     void Start()
     {
         healtBar = GetComponent<Image>();
         HP = maxHP;
+        //if (deathCanvas != null)
+        //{
+        //    deathCanvas.gameObject.SetActive(false);
+        //}
     }
 
     // Update is called once per frame
     void Update()
     {
         healtBar.fillAmount = HP / maxHP ;
+        if (HP <= 0 )
+        {
+            //if (deathCanvas != null)
+            //{
+            //    deathCanvas.gameObject.SetActive(true);
+            //    Time.timeScale = 0f;
+            //}
+            //if (alreadyDied) return;
+           // alreadyDied = true;
+
+        }
     }
 }
